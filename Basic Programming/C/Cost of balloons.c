c92a0e7bcfff892dbac3dd2346553e5b55ed2dbd
Syed Mohammed Zainul Aabdeen <83681938+Zainulaabdeen-786@users.noreply.github.com>
2023-08-03 20:49:06 +0530

    Create Cost of balloons.c

---
+++ Basic Programming/C/Cost of balloons.c
@@ -0,0 +1,66 @@
+/*Problem
+You are conducting a contest at your college. This contest consists of two problems and n participants. You know the problem that a candidate will solve during the contest.
+You provide a balloon to a participant after he or she solves a problem. There are only green and purple-colored balloons available in a market. Each problem must have a balloon associated with it as a prize for solving that specific problem. You can distribute balloons to each participant by performing the following operation:
+    Use green-colored balloons for the first problem and purple-colored balloons for the second problem
+    Use purple-colored balloons for the first problem and green-colored balloons for the second problem
+You are given the cost of each balloon and problems that each participant solve. Your task is to print the minimum price that you have to pay while purchasing balloons.
+
+Input format
+First line: T that denotes the number of test cases (1 <= T <= 10)
+For each test case: 
+    First line: Cost of green and purple-colored balloons 
+    Second line: n that denotes the number of participants (1 <= n <= 10)
+Next n lines: Contain the status of users. For example, if the value of the jth integer in the ith row is 0, then it depicts that the ith participant has not solved the jth problem. Similarly, if the value of the jth integer in the ith row is 1, then it depicts that the ith participant has solved the jth problem.
+Output format
+For each test case, print the minimum cost that you have to pay to purchase balloons.
+
+Sample Input
+2
+9 6
+10
+1 1
+1 1
+0 1
+0 0
+0 1
+0 0
+0 1
+0 1
+1 1
+0 0
+1 9
+10
+0 1
+0 0
+0 0
+0 1
+1 0
+0 1
+0 1
+0 0
+0 1
+0 0
+Sample Output
+69
+14        */
+
+#include <stdio.h>
+int main(){
+	int T, green, purple, n;
+	scanf("%d", &T);
+	for(int i=1; i<=T; i++){
+		scanf("%d %d", &green, &purple);
+		scanf("%d", &n);
+		int a1, a2, sum1=0, sum2=0;
+		for(int j=0; j<n; j++){
+			scanf("%d %d", &a1, &a2);
+			sum1 += a1;
+			sum2 += a2;
+		}
+		a1 = sum1*green + sum2*purple;
+		a2 = sum2*green + sum1*purple;
+		if(a1 < a2)	printf("%d\n", a1);
+		else	printf("%d\n", a2);
+	}
+	return 0;
+}
